9ff78105af7e4b6c0528f5cf1d76fe8f94edde20
CamiKaze <cameronsmith63@gmail.com>
2019-07-08 20:36:26 +0200

    First commit

---
+++ ReceiveFromRabbitMQ/ReceiveFromRabbitMQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace ReceiveFromRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // Read specific ID from queue
+            Receive("MSGID");
+            Console.ReadLine();
+        }
+
+        public static void Receive(string queue)
+        { // Receive message and print
+            using (IConnection connection = new ConnectionFactory().CreateConnection())
+            { // Create and use rabbitMQ connection details
+                using (IModel channel = connection.CreateModel())
+                { // Declare a queue, Dispatch deliveries, Check queue for data
+                    channel.QueueDeclare(queue, false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    BasicGetResult result = channel.BasicGet(queue, true);
+                    if (result == null)
+                        Console.WriteLine("No messages found.");
+                    else
+                    {
+                        string data = Encoding.UTF8.GetString(result.Body);
+                        var Name = GetName(data);
+                        Console.WriteLine($"Hello {Name}, I am your Father!");
+                    }
+                }
+            }
+        }
+
+        private static string GetName(string data)
+        {
+            string Name = data.Split(' ').Last();
+            return Name;
+        }
+    }
+}
---
+++ SendToRabbitMQ/SendToRabbitMQ/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace SendToRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { // Get name from user, send message ID and string to Send Method
+            Console.WriteLine("What is your name?");
+            string Name = Console.ReadLine();
+            Send("MSGID", $"Hello my name is {Name}");
+        }
+        public static void Send(string queue, string data)
+        { // Accept message ID and string
+            using (IConnection connection = new ConnectionFactory().CreateConnection())
+            { // Create and use rabbitMQ connection details
+                using (IModel channel = connection.CreateModel())
+                {   // Name of queue, Publish message with mandatory and immediate flags set to false
+                    channel.QueueDeclare(queue, false, false, false, null);
+                    channel.BasicPublish(string.Empty, queue, null, Encoding.UTF8.GetBytes(data));
+                    Console.WriteLine($"Message Sent: {data}");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
